a23987153def6959190e0f6f5e0e923cc8885e42
sahil6900 <sahilpawar33@gmail.com>
2022-07-26 23:33:27 +0530

    Sahil-created user entity

---
+++ src/main/java/com/blog/app/entities/User.java
@@ -0,0 +1,23 @@
+package com.blog.app.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String lastName;
+}
